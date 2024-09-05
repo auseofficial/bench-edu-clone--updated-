e0f0e85908dccde824b9e930a4a316b341d90cb0
Akib Us Suny Eshan <eshan.r029@gmail.com>
2024-09-05 16:16:49 +0600

    updated form

--- school/school/urls.py
+++ school/school/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 admin.site.site_header = "School Management System Clone "
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('academic/', include('academic.urls')),
